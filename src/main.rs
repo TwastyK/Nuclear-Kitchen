fcc16dd1d1cc1fb742839406d06d990dbea00c57
unknown <creatorgamedev897@gmail.com>
2026-05-26 01:43:09 +0300

    3d changes after installing nvidia driver on my laptop

--- src/main.rs
+++ src/main.rs
@@ -35,19 +35,20 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_plugins(bevy::diagnostic::LogDiagnosticsPlugin::default())
         .add_systems(Startup, setup)
-        //.add_systems(Update, (cook_meat, move_player, check_oven, take_food, delivery_food))
+        .add_systems(Update, (cook_meat, move_player, check_oven, take_food, delivery_food))
         .insert_resource(Score { score: 0 })
         .run();
 }
 #[derive(Component)]
 struct FoodTimer(f32);
 
-const OVEN_SIZE: Vec2 = Vec2::new(500.0, 100.0);
-const CUSTOMER_SIZE: Vec2 = Vec2::new(75.0, 75.0);
+const OVEN_SIZE: Vec3 = Vec3::new(50.0, 10.0, 50.0);
+const CUSTOMER_SIZE: Vec3 = Vec3::new(75.0, 90.0, 85.0);
 
-fn in_bounds(pos: Vec2, center: Vec2, size: Vec2) -> bool {
+fn in_bounds(pos: Vec3, center: Vec3, size: Vec3) -> bool {
     pos.x > center.x - size.x / 2.0 && pos.x < center.x + size.x / 2.0
-        && pos.y > center.y - size.y / 2.0 && pos.y < center.y + size.y / 2.0
+        && pos.y > center.y - size.y / 2.0 && pos.y < center.y + size.y / 2.0 && pos.z > center.z - size.z / 2.0 &&
+        pos.z < center.z + size.z / 2.0
 }
 
 fn setup(mut commands: Commands,
@@ -71,36 +72,47 @@ fn setup(mut commands: Commands,
     ));
 
     commands.spawn((
-        Mesh3d(meshes.add(Cuboid::new(2.0, 2.0, 2.0))),
+        Mesh3d(meshes.add(Cuboid::new(2.0, 1.0, 2.0))),
         MeshMaterial3d(materials.add(StandardMaterial {
-            base_color: Color::srgb(1.0, 0.0, 0.0),
+            base_color: Color::srgb(1.0, 1.0, 0.0),
             unlit: true,
             ..default()
         })),
         Transform::from_xyz(0.0, 0.0, -5.0),
         Visibility::Visible,
+        Oven
     ));
+
+    commands.spawn((
+        Mesh3d(meshes.add(Cuboid::new(2.0, 3.0, 2.0))),
+        MeshMaterial3d(materials.add(StandardMaterial {
+            base_color: Color::srgb(0.0, 1.0, 1.0),
+            ..default()
+        })),
+        Transform::from_xyz(0.0, 0.0, 5.0),
+        Player { speed: 1.0, taked_food: None },
+        ));
 }
 
 fn cook_meat(
     time: Res<Time>,
     mut commands: Commands,
     keys: Res<ButtonInput<KeyCode>>,
-    mut query: Query<(&mut FoodTimer, &mut food_states, &mut Sprite), With<FoodTimer>>,
+    mut query: Query<(&mut FoodTimer, &mut food_states, &mut Mesh3d), With<FoodTimer>>,
     player: Query<&Transform, With<Player>>,
     oven: Query<&Transform, With<Oven>>,
 ) {
     for transform in oven {
         for player in player {
-            if in_bounds(player.translation.truncate(), transform.translation.truncate(), OVEN_SIZE)
+            if in_bounds(player.translation, transform.translation, OVEN_SIZE)
             {
                 if keys.just_pressed(KeyCode::Space) {
                     commands.spawn((
-                        Sprite {
-                            color: Color::srgb_u8(255, 192, 203),
-                            custom_size: Some(Vec2::new(100.0, 100.0)),
+                        Mesh3d(meshes.add(Cuboid::new(2.0, 2.0, 2.0))),
+                        MeshMaterial3d(materials.add(StandardMaterial {
+                            base_color: Color::srgb(5.0, 6.0, 8.0),
                             ..default()
-                        },
+                        })),
                         Transform::from_translation(player.translation),
                         FoodTimer(0.0),
                         FoodType::Steak,
@@ -109,13 +121,13 @@ fn cook_meat(
                 }
             }
         }
-        for (mut timer, mut states, mut sprite) in &mut query {
+        for (mut timer, mut states, mut mesh) in &mut query {
             timer.0 += time.delta_secs();
             match *states {
                 food_states::Raw => {
                     if timer.0 >= 5.0 {
                         *states = food_states::Cooked;
-                        sprite.color = Color::srgb(0.4, 0.2, 0.0);
+                        mesh.base_color = Color::srgb(0.4, 0.2, 0.0);
                         println!("Cooked food");
                     }
                 }
@@ -135,16 +147,16 @@ fn move_player(
 ) {
     for (mut transform, player) in &mut query {
         if keys.pressed(KeyCode::KeyW) {
-            transform.translation.y += player.speed * time.delta_secs();
-        }
-        if keys.pressed(KeyCode::KeyS) {
-            transform.translation.y -= player.speed * time.delta_secs();
-        }
-        if keys.pressed(KeyCode::KeyD) {
             transform.translation.x += player.speed * time.delta_secs();
         }
-        if keys.pressed(KeyCode::KeyA) {
+        if keys.pressed(KeyCode::KeyS) {
             transform.translation.x -= player.speed * time.delta_secs();
+        }
+        if keys.pressed(KeyCode::KeyD) {
+            transform.translation.z += player.speed * time.delta_secs();
+        }
+        if keys.pressed(KeyCode::KeyA) {
+            transform.translation.z -= player.speed * time.delta_secs();
         }
     }
 }
@@ -152,7 +164,7 @@ fn move_player(
 fn check_oven(player: Query<&Transform, With<Player>>, oven: Query<&Transform, With<Oven>>) {
     for transform in oven {
         for player in player {
-            if in_bounds(player.translation.truncate(), transform.translation.truncate(), OVEN_SIZE) {
+            if in_bounds(player.translation, transform.translation, OVEN_SIZE) {
 
             }
         }
@@ -165,7 +177,7 @@ fn take_food(mut commands: Commands, keys: Res<ButtonInput<KeyCode>>,
                 for (food_transform, food_states, food, entity) in &food {
                     if *food_states == food_states::Cooked {
                         for mut transform in &mut player {
-                            if in_bounds(transform.0.translation.truncate(), food_transform.translation.truncate(), CUSTOMER_SIZE){
+                            if in_bounds(transform.0.translation, food_transform.translation, CUSTOMER_SIZE){
                                     if keys.just_pressed(KeyCode::KeyE) {
                                         commands.entity(entity).despawn();
                                             transform.1.taked_food = Some(*food);
@@ -183,7 +195,7 @@ fn delivery_food(customer: Query<(&Transform, &Customer, Entity)>,
                  keys: Res<ButtonInput<KeyCode>>) {
     for (transform, customer, entity) in customer {
         for mut player in &mut player {
-            if in_bounds(player.0.translation.truncate(), transform.translation.truncate(), CUSTOMER_SIZE) {
+            if in_bounds(player.0.translation, transform.translation, CUSTOMER_SIZE) {
                 if player.1.taked_food == Some(customer.order) {
                     if keys.just_pressed(KeyCode::KeyE) {
                         score.score += 1;
